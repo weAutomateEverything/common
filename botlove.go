03b51b139f7d283cacbdd927c16e9877ec2e2de8
Marc Arndt <marc@marcarndt.com>
2018-12-08 21:19:32 +0200

    fixing lambda error

--- botlove.go
+++ botlove.go
@@ -10,8 +10,9 @@ import (
 )
 
 func LogLambdaError(err error) {
-	log.Printf("ERROR: %v -  %v", os.Getenv("AWS_LAMBDA_FUNCTION_NAME"), err.Error())
-	resp, err := http.Post("http://api.carddevops.co.za/telegram/error", "application/text", strings.NewReader(fmt.Sprintf("*%v*\n\n%v", err.Error())))
+	msg := fmt.Sprintf("ERROR: %v -  %v", os.Getenv("AWS_LAMBDA_FUNCTION_NAME"), err.Error())
+	log.Printf(msg)
+	resp, err := http.Post("http://api.carddevops.co.za/telegram-text/1083240170", "application/text", strings.NewReader(msg))
 	if err == nil {
 		resp.Body.Close()
 	} else {
