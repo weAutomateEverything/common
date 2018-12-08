b903590634a5a9f4d6a09b57cbfc4cf01a332e49
Marc Arndt <marc@marcarndt.com>
2018-12-08 22:29:12 +0200

    fixing alert endpoint

--- botlove.go
+++ botlove.go
@@ -12,7 +12,7 @@ import (
 func LogLambdaError(err error) {
 	msg := fmt.Sprintf("ERROR: %v -  %v", os.Getenv("AWS_LAMBDA_FUNCTION_NAME"), err.Error())
 	log.Printf(msg)
-	resp, err := http.Post("http://api.carddevops.co.za/telegram-text/1083240170", "application/text", strings.NewReader(msg))
+	resp, err := http.Post("https://api.carddevops.co.za/telegram-text/1083240170", "application/text", strings.NewReader(msg))
 	if err == nil {
 		resp.Body.Close()
 	} else {
@@ -25,7 +25,7 @@ func LogLambdaError(err error) {
 func LogLambdaErrorMsg(id string, err error) {
 	msg := fmt.Sprintf("ERROR: %v - %v\n %v", os.Getenv("AWS_LAMBDA_FUNCTION_NAME"),id, err.Error())
 	log.Printf(msg)
-	resp, err := http.Post("http://api.carddevops.co.za/telegram-text/1083240170", "application/text", strings.NewReader(msg))
+	resp, err := http.Post("https://api.carddevops.co.za/telegram-text/1083240170", "application/text", strings.NewReader(msg))
 	if err == nil {
 		resp.Body.Close()
 	} else {
